3a4a97dbb15a299e06f9b552fde39c5aa5e78b93
benno <bennoosterholt@gmail.com>
2019-04-09 21:02:09 +0200

    addde the command used. priviledge esc no work yet

--- lights/commands-at-the-moment.bash
+++ lights/commands-at-the-moment.bash
@@ -1,3 +1,6 @@
 
 # dependences werk now, a dash in front was missing
 ansible-playbook -i inventory/inventory.yml site.yml --ask-become-pass
+
+# for VMs on Finch
+ansible-playbook -i inventory/inventory.finch.yml northern-lights.yml --ask-become-pass
