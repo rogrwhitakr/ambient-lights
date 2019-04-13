24c1f5fcaf40cc281a2e0631308ebdd962189459
benno <bennoosterholt@gmail.com>
2019-04-13 20:44:38 +0200

    ok

--- lights/commands-at-the-moment.bash
+++ lights/commands-at-the-moment.bash
@@ -4,3 +4,9 @@ ansible-playbook -i inventory/inventory.yml site.yml --ask-become-pass
 
 # for VMs on Finch
 ansible-playbook -i inventory/inventory.finch.yml northern-lights.yml --ask-become-pass
+
+# check if it works, it does... unsure what to do
+ansible -i inventory/inventory.finch.yml server_auth -m setup --ask-become-pass
+
+# this dont work
+ansible-playbook -i inventory/inventory.finch.yml northern-lights.yml --extra-vars "ansible_become_pass=vasques"
